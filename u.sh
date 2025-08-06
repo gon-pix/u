aba6b9276732d00b3f71738a43aef9a560a05479
gon-pix <gonp.oly.83@gmail.com>
2025-08-07 06:13:07 +0700

    Create u.sh

---
+++ u.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install gnome-session gnome-tweaks` gdm3 gnome-terminal nautilus gnome-shell-extensions gnome-shell-extension-ubuntu-dock yaru-theme-gtk yaru-theme-icon
+sudo snap install firefox
